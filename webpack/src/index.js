d2306ff995aa5cb30032d2ad072d4add93260471
cloudtian <cloud_tian@foxmail.com>
2018-11-07 22:20:51 +0800

    [add][webpack]管理资源，加载css,图片，字体等

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import './asset/font/iconfont.css';
+import './index.css';
+import catImage from './asset/image/cat.jpg';
+
 
 function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
+    // 将图像添加到我们现有的div
+    var myIcon = new Image();
+    myIcon.src = catImage;
+
+    element.appendChild(myIcon);
+  
     return element;
 }
 document.body.appendChild(component());
