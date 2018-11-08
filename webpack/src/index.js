0f4d9dbca3f8769a0bf87e591c37574539ed61d6
cloudtian <cloud_tian@foxmail.com>
2018-11-08 22:30:03 +0800

    [update][webpack]管理输出，通过webpack相关插件进行管理构建输出

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,19 +2,23 @@ import _ from 'lodash';
 import './asset/font/iconfont.css';
 import './index.css';
 import catImage from './asset/image/cat.jpg';
-
+import printMe from './print';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    // element.classList.add('hello');
 
     // 将图像添加到我们现有的div
-    var myIcon = new Image();
-    myIcon.src = catImage;
+    // var myIcon = new Image();
+    // myIcon.src = catImage;
 
-    element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
   
     return element;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
 
+// 如果修改了入口起点的名称或添加新的名称，生成的包将被重命名在一个构建中，但是index.html文件仍然会引用旧的名字
+// HtmlWebpackPlugin 会默认生成index.html文件替换已有的
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// build前先清理dist文件夹，然后再生成新的
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// 用于跟踪模块映射到输出bundle的过程
+const WebpackManifestPlugin = require('webpack-manifest-plugin');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new WebpackManifestPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
